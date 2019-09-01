d58e11c85bddcdbf96070aa48b98a6e820364b78
thzw1142 <thzw1142@163.com>
2019-09-01 15:33:06 +0800

    adapt layout

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/sunny-everyday/GOGUIforTCPsendMsg
+
+require (
+	github.com/lxn/walk v0.0.0-20190829150329-026611184a72
+	github.com/lxn/win v0.0.0-20190716185335-d1d36f0e4f48 // indirect
+	golang.org/x/sys v0.0.0-20190830142957-1e83adbbebd0 // indirect
+	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
+	"io/ioutil"
 	"net"
 	"path/filepath"
-	"io/ioutil"
-	"strings"
 	"strconv"
-	"encoding/xml"
+	"strings"
 )
 
 type Condom struct {
-	Index   int
-	Name    string
-	Type    string
+	Index       int
+	Name        string
+	Type        string
 	MessageInfo string
-	checked bool
+	checked     bool
 }
 
 type CondomModel struct {
@@ -55,9 +55,9 @@ func (m *CondomModel) Checked(row int) bool {
 func (m *CondomModel) GetCheckedItemlist() []int {
 	var j int
 	var checkedlist []int
-	length :=len(m.items)
+	length := len(m.items)
 	for row := 0; row < length; row++ {
-		if m.Checked(row){
+		if m.Checked(row) {
 			checkedlist[j] = row
 		}
 	}
@@ -69,29 +69,27 @@ func (m *CondomModel) SetChecked(row int, checked bool) error {
 	return nil
 }
 
-
 func (m *CondomModel) Len() int {
 	return len(m.items)
 }
 
-
 func (m *CondomModel) Swap(i, j int) {
 	m.items[i], m.items[j] = m.items[j], m.items[i]
 }
-func (m *CondomModel)FloatToString(input_num float64) string {
+func (m *CondomModel) FloatToString(input_num float64) string {
 	// to convert a float number to a string
 	return strconv.FormatFloat(float64(input_num), 'f', 6, 64)
 }
+
 //message number
-var osmessagenumber int  = 0
-var rsmessagenumber int  = 0
+var osmessagenumber int = 0
+var rsmessagenumber int = 0
 
 var onlysendmessagelist [100]string
 var readsendmessagelist [100]string
 
 var onlysendmessage [100]string
 var readsendmessage [100]string
-
 
 func NewCondomModel() *CondomModel {
 	m := new(CondomModel)
@@ -108,24 +106,23 @@ type CondomMainWindow struct {
 
 func (m *CondomMainWindow) ResetRows() {
 	items := []*Condom{}
-	
 
-	for i := 0; i<osmessagenumber ; i++ {
-		x:= &Condom{
-			Index: i+1,
-			Name:   onlysendmessagelist[i],
-			Type:   "send",
-			MessageInfo:  onlysendmessage[i],
+	for i := 0; i < osmessagenumber; i++ {
+		x := &Condom{
+			Index:       i + 1,
+			Name:        onlysendmessagelist[i],
+			Type:        "send",
+			MessageInfo: onlysendmessage[i],
 		}
 		items = append(items, x)
 	}
 
-	for j := osmessagenumber; j < osmessagenumber +rsmessagenumber; j++ {
-		x:= &Condom{
-			Index: j+1,
-			Name:   readsendmessagelist[j-osmessagenumber],
-			Type:   "receive",
-			MessageInfo:  readsendmessage[j-osmessagenumber],
+	for j := osmessagenumber; j < osmessagenumber+rsmessagenumber; j++ {
+		x := &Condom{
+			Index:       j + 1,
+			Name:        readsendmessagelist[j-osmessagenumber],
+			Type:        "receive",
+			MessageInfo: readsendmessage[j-osmessagenumber],
 		}
 		items = append(items, x)
 	}
@@ -135,172 +132,179 @@ func (m *CondomMainWindow) ResetRows() {
 }
 func main() {
 	mw := &CondomMainWindow{model: NewCondomModel()}
-	var IP, Port *walk.TextEdit
+	var IP, Port *walk.LineEdit
 	var lnkclient net.Conn
 	var lnkclientconnectFlag bool = false
-	tcpdisconnect :=make(chan bool)
-	
+	tcpdisconnect := make(chan bool)
+
 	MainWindow{
 		AssignTo: &mw.MainWindow,
 		Title:    "Robot Simulator",
 		Size:     Size{800, 500},
 		Layout:   VBox{},
-
 		Children: []Widget{
-			Composite{
-				Layout: HBox{MarginsZero: true},
-				Children: []Widget{
-					PushButton{
-						Text: "Reload",
-						OnClicked: func() {
-							fpt, err := filepath.Abs("go_build_minfang827.exe")
-							if err != nil {
-								panic(err)
-							}
-							fmt.Println(fpt)
-							way1 := strings.Replace(fpt, "go_build_minfang827.exe", `Config\onlysend\`, 1)
-							way2 := strings.Replace(fpt, "go_build_minfang827.exe", `Config\readsend\`, 1)
-
-
-							//onlysend
-							files, _ := ioutil.ReadDir(way1)
-							fmt.Println(way1)
-							osmessagenumber = 0
-							for _, f := range files {
-								fmt.Println(f.Name())
-								onlysendmessagelist[osmessagenumber] = f.Name()
-								data, _ := ioutil.ReadFile(way1+f.Name())
-								fmt.Println(data)
-								onlysendmessage[osmessagenumber]  = string(data)
-								osmessagenumber ++
-							}
-							//readsend
-							files, _ = ioutil.ReadDir(way2)
-							fmt.Println(way2)
-							rsmessagenumber = 0
-							for _, f := range files {
-								fmt.Println(f.Name())
-								readsendmessagelist[rsmessagenumber] = f.Name()
-								data, _ := ioutil.ReadFile(way2+f.Name())
-								fmt.Println(data)
-								readsendmessage[rsmessagenumber] = string(data)
-								rsmessagenumber ++
-							}
-							fmt.Println(rsmessagenumber +osmessagenumber)
-							mw.ResetRows()
-						},
-
-					},
-					PushButton{
-						Text: "Connect",
-						OnClicked: func() {
-							serveraddr := IP.Text() + ":" + Port.Text()
-							conn, err := net.Dial("tcp", serveraddr)
-
-							if err != nil {
-								fmt.Printf("connect failed, err : %v\n", err.Error())
-								return
-							}
-							lnkclient = conn
-							lnkclientconnectFlag = true
-							go mw.TcpClientReadandSend(tcpdisconnect,lnkclient,&lnkclientconnectFlag)
-						},
-					},
-					PushButton{
-						Text: "Send",
-						OnClicked: func() {
-							for _, x := range mw.model.items {
-								if x.checked {
-									fmt.Printf("checked: %v\n", x)
-									var err error
-									_,err = lnkclient.Write([]byte(x.MessageInfo))
-
-									if err != nil {
-										fmt.Printf("write failed , err : %v\n", err)
-										break
-									}
-								}
-							}
-							fmt.Println()
-
-						},
-					},
-					PushButton{
-						Text: "disconnect",
-						OnClicked: func() {
-							if(lnkclientconnectFlag){
-								go disconnect(tcpdisconnect,lnkclient)
-							}
-							
-						},
-					},
-				},
-			},
-			Composite{
-				Layout: HBox{MarginsZero: true},
-				Children: []Widget{
-					HSplitter{
+			TabWidget{
+				Pages: []TabPage{
+					{
+						Title:  "Robot",
+						Layout: VBox{},
 						Children: []Widget{
-							Label{
-								Text: "IP",
+							GroupBox{
+								Layout: HBox{MarginsZero: true},
+								Children: []Widget{
+									PushButton{
+										Text: "Reload",
+										OnClicked: func() {
+											fpt, err := filepath.Abs("go_build_minfang827.exe")
+											if err != nil {
+												panic(err)
+											}
+											fmt.Println(fpt)
+											way1 := strings.Replace(fpt, "go_build_minfang827.exe", `Config\onlysend\`, 1)
+											way2 := strings.Replace(fpt, "go_build_minfang827.exe", `Config\readsend\`, 1)
+
+											//onlysend
+											files, _ := ioutil.ReadDir(way1)
+											fmt.Println(way1)
+											osmessagenumber = 0
+											for _, f := range files {
+												fmt.Println(f.Name())
+												onlysendmessagelist[osmessagenumber] = f.Name()
+												data, _ := ioutil.ReadFile(way1 + f.Name())
+												fmt.Println(data)
+												onlysendmessage[osmessagenumber] = string(data)
+												osmessagenumber ++
+											}
+											//readsend
+											files, _ = ioutil.ReadDir(way2)
+											fmt.Println(way2)
+											rsmessagenumber = 0
+											for _, f := range files {
+												fmt.Println(f.Name())
+												readsendmessagelist[rsmessagenumber] = f.Name()
+												data, _ := ioutil.ReadFile(way2 + f.Name())
+												fmt.Println(data)
+												readsendmessage[rsmessagenumber] = string(data)
+												rsmessagenumber ++
+											}
+											fmt.Println(rsmessagenumber + osmessagenumber)
+											mw.ResetRows()
+										},
+									},
+									PushButton{
+										Text: "Connect",
+										OnClicked: func() {
+											serveraddr := IP.Text() + ":" + Port.Text()
+											conn, err := net.Dial("tcp", serveraddr)
+
+											if err != nil {
+												fmt.Printf("connect failed, err : %v\n", err.Error())
+												return
+											}
+											lnkclient = conn
+											lnkclientconnectFlag = true
+											go mw.TcpClientReadandSend(tcpdisconnect, lnkclient, &lnkclientconnectFlag)
+										},
+									},
+									PushButton{
+										Text: "Send",
+										OnClicked: func() {
+											for _, x := range mw.model.items {
+												if x.checked {
+													fmt.Printf("checked: %v\n", x)
+													var err error
+													_, err = lnkclient.Write([]byte(x.MessageInfo))
+
+													if err != nil {
+														fmt.Printf("write failed , err : %v\n", err)
+														break
+													}
+												}
+											}
+											fmt.Println()
+
+										},
+									},
+									PushButton{
+										Text: "disconnect",
+										OnClicked: func() {
+											if (lnkclientconnectFlag) {
+												go disconnect(tcpdisconnect, lnkclient)
+											}
+
+										},
+									},
+								},
 							},
-							TextEdit{AssignTo: &IP},
-							Label{
-								Text: "PORT",
+							Composite{
+								Layout: HBox{MarginsZero: true},
+								Children: []Widget{
+									Label{
+										Text: "IP",
+									},
+									LineEdit{
+										AssignTo: &IP,
+									},
+									Label{
+										Text: "PORT",
+									},
+									LineEdit{AssignTo: &Port},
+								},
 							},
-							TextEdit{AssignTo: &Port},
-						},
-					},
+							Composite{
+								Layout: VBox{},
+								ContextMenuItems: []MenuItem{
+									Action{
+										Text:        "I&nfo",
+										OnTriggered: mw.tv_ItemActivated,
+									},
+									Action{
+										Text: "E&xit",
+										OnTriggered: func() {
+											lnkclient.Close()
+											mw.Close()
+										},
+									},
+								},
+								Children: []Widget{
+									TableView{
+										AssignTo:         &mw.tv,
+										CheckBoxes:       true,
+										ColumnsOrderable: true,
+										MultiSelection:   true,
+										Columns: []TableViewColumn{
+											{Title: "编号"},
+											{Title: "消息名称"},
+											{Title: "消息类型"},
+											{Title: "消息内容"},
+										},
+										Model: mw.model,
+										OnCurrentIndexChanged: func() {
+											i := mw.tv.CurrentIndex()
+											if 0 <= i {
+												fmt.Printf("OnCurrentIndexChanged: %v\n", mw.model.items[i].Name)
 
-				},
-			},
-			Composite{
-				Layout: VBox{},
-				ContextMenuItems: []MenuItem{
-					Action{
-						Text:        "I&nfo",
-						OnTriggered: mw.tv_ItemActivated,
-					},
-					Action{
-						Text: "E&xit",
-						OnTriggered: func() {
-						    lnkclient.Close()
-							mw.Close()
+											}
+										},
+										OnSelectedIndexesChanged: func() {
+											fmt.Printf("SelectedIndexes: %v\n", mw.tv.SelectedIndexes())
+										},
+										OnItemActivated: mw.tv_ItemActivated,
+									},
+								},
+							},
 						},
 					},
-				},
-				Children: []Widget{
-					TableView{
-						AssignTo:         &mw.tv,
-						CheckBoxes:       true,
-						ColumnsOrderable: true,
-						MultiSelection:   true,
-						Columns: []TableViewColumn{
-							{Title: "编号"},
-							{Title: "消息名称"},
-							{Title: "消息类型"},
-							{Title: "消息内容"},
-						},
-						Model: mw.model,
-						OnCurrentIndexChanged: func() {
-							i := mw.tv.CurrentIndex()
-							if 0 <= i {
-								fmt.Printf("OnCurrentIndexChanged: %v\n", mw.model.items[i].Name)
-
-							}
-						},
-						OnSelectedIndexesChanged: func() {
-							fmt.Printf("SelectedIndexes: %v\n", mw.tv.SelectedIndexes())
-						},
-						OnItemActivated: mw.tv_ItemActivated,
+					{
+						Title:  "104",
+						Layout: VBox{},
 					},
-
 				},
 			},
 		},
 	}.Run()
 }
-func disconnect(ch chan bool,lnkclient net.Conn){
+func disconnect(ch chan bool, lnkclient net.Conn) {
 	//lnkclient.Close()
 	//lnkclientconnectFlag = false
 	ch <- true
@@ -314,14 +318,14 @@ func (mw *CondomMainWindow) tv_ItemActivated() {
 	walk.MsgBox(mw, "title", msg, walk.MsgBoxIconInformation)
 }
 
-func (mw *CondomMainWindow) TcpClientReadandSend(ch chan bool,lnkclient net.Conn,lnkclientconnectFlag *bool){
+func (mw *CondomMainWindow) TcpClientReadandSend(ch chan bool, lnkclient net.Conn, lnkclientconnectFlag *bool) {
 	var disconnectflag bool
 	for {
 		disconnectflag = <-ch
-		if(disconnectflag){
+		if (disconnectflag) {
 			lnkclient.Close()
 			*lnkclientconnectFlag = false
-			return 
+			return
 		}
 		var buf [2000]byte
 		n, err := lnkclient.Read(buf[:])
@@ -333,61 +337,61 @@ func (mw *CondomMainWindow) TcpClientReadandSend(ch chan bool,lnkclient net.Conn
 		str := string(buf[:n])
 		fmt.Printf("receive from client, data: %v\n", str)
 		sendmessageName := mw.GetXMLanswer(str)
-		if("" != sendmessageName){
+		if ("" != sendmessageName) {
 			for _, x := range mw.model.items {
 				if x.Name == sendmessageName {
 					var err error
-					_,err = lnkclient.Write([]byte(x.MessageInfo))
+					_, err = lnkclient.Write([]byte(x.MessageInfo))
 					if err != nil {
 						fmt.Printf("write failed , err : %v\n", err)
 						break
 					}
 				}
 			}
-		}	
+		}
 	}
 }
-func (mw *CondomMainWindow) GetXMLanswer(receiveXML string)string {
+func (mw *CondomMainWindow) GetXMLanswer(receiveXML string) string {
 	var t xml.Token
-    var err error
-    inputReader := strings.NewReader(receiveXML)
+	var err error
+	inputReader := strings.NewReader(receiveXML)
 
-    // 从文件读取，如可以如下：
-    // content, err := ioutil.ReadFile("studygolang.xml")
-    // decoder := xml.NewDecoder(bytes.NewBuffer(content))
+	// 从文件读取，如可以如下：
+	// content, err := ioutil.ReadFile("studygolang.xml")
+	// decoder := xml.NewDecoder(bytes.NewBuffer(content))
 
-    decoder := xml.NewDecoder(inputReader)
-    var TYPEflag bool
-    for t,err = decoder.Token(); err == nil; t,err = decoder.Token() {
-        switch token := t.(type) {
-        // 处理元素开始（标签）
-        case xml.StartElement:
-            name := token.Name.Local
-            fmt.Printf("Token name: %s\n", name)
-            if("Type" == name) {
+	decoder := xml.NewDecoder(inputReader)
+	var TYPEflag bool
+	for t, err = decoder.Token(); err == nil; t, err = decoder.Token() {
+		switch token := t.(type) {
+		// 处理元素开始（标签）
+		case xml.StartElement:
+			name := token.Name.Local
+			fmt.Printf("Token name: %s\n", name)
+			if ("Type" == name) {
 				TYPEflag = true
 			}
-            for _, attr := range token.Attr {
-                attrName := attr.Name.Local
-                attrValue := attr.Value
-                fmt.Printf("An attribute is: %s %s\n", attrName, attrValue)
-            }
-        // 处理元素结束（标签）
-        case xml.EndElement:
-            fmt.Printf("Token of '%s' end\n", token.Name.Local)
-        // 处理字符数据（这里就是元素的文本）
-        case xml.CharData:
-            if(TYPEflag){
+			for _, attr := range token.Attr {
+				attrName := attr.Name.Local
+				attrValue := attr.Value
+				fmt.Printf("An attribute is: %s %s\n", attrName, attrValue)
+			}
+		// 处理元素结束（标签）
+		case xml.EndElement:
+			fmt.Printf("Token of '%s' end\n", token.Name.Local)
+		// 处理字符数据（这里就是元素的文本）
+		case xml.CharData:
+			if (TYPEflag) {
 				content := string([]byte(token))
 				fmt.Printf("This is the content: %v\n", content)
-				if("1" == content){
+				if ("1" == content) {
 					return "control58Res"
 				}
 			}
-			TYPEflag=false
-        default:
-            // ...
-        }
-    }
+			TYPEflag = false
+		default:
+			// ...
+		}
+	}
 	return ""
 }
